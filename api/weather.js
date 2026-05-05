6c2998195a0f27080242922f6420aa370412f8ce
hchkim0217-cell <hchkim0217@gmail.com>
2026-05-05 20:33:14 +0900

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,16 +1,33 @@
 export default async function handler(req, res) {
-  const { nx, ny, base_date, base_time } = req.query;
-  const API_KEY = process.env.KMA_API_KEY;
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const KEY = process.env.KMA_API_KEY;
+  const { type, nx, ny, base_date, base_time, regId, tmFc } = req.query;
 
-  const url = `https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getVilageFcst` +
-    `?pageNo=1&numOfRows=1000&dataType=JSON` +
-    `&base_date=${base_date}&base_time=${base_time}` +
-    `&nx=${nx}&ny=${ny}&authKey=${API_KEY}`;
+  let url = '';
+
+  if (type === 'short') {
+    // 단기예보 (오늘~모레)
+    url = `https://apihub.kma.go.kr/api/typ02/openApi/VilageFcstInfoService_2.0/getVilageFcst`
+      + `?pageNo=1&numOfRows=1000&dataType=JSON`
+      + `&base_date=${base_date}&base_time=${base_time}`
+      + `&nx=${nx}&ny=${ny}&authKey=${KEY}`;
+
+  } else if (type === 'mid_land') {
+    // 중기육상예보 (4~10일 강수확률)
+    url = `https://apihub.kma.go.kr/api/typ02/openApi/MidFcstInfoService/getMidLandFcst`
+      + `?pageNo=1&numOfRows=10&dataType=JSON`
+      + `&regId=${regId}&tmFc=${tmFc}&authKey=${KEY}`;
+
+  } else if (type === 'mid_temp') {
+    // 중기기온예보 (4~10일 기온)
+    url = `https://apihub.kma.go.kr/api/typ02/openApi/MidFcstInfoService/getMidTa`
+      + `?pageNo=1&numOfRows=10&dataType=JSON`
+      + `&regId=${regId}&tmFc=${tmFc}&authKey=${KEY}`;
+  }
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (e) {
     res.status(500).json({ error: e.message });
